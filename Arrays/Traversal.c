8ea4940a1365110bb3608281d86666b599d1f4ae
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2025-01-24 14:08:54 +0530

    Arrays

--- Arrays/Traversal.c
+++ Arrays/Traversal.c
@@ -1,16 +1,13 @@
-#include <iostream>
-using namespace std;
-
+#include<stdio.h>
 int main() 
 {
    int n;
    int arr[10] ={4,5,6,3,2,1,3,45,66,90};
 
-  // Calculate size of an array by using any of below methods:
+  // Calculate size of an array by using below method:
 
   // 1. sizeof(arr)/sizeof(arr[0]) i.e.(total array size)/(single element of an array)
-  //  OR
-  // 2. size(arr) for c++ 17 and above
+  
   // CODE HERE
   
   
